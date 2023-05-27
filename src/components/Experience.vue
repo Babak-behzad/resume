e97f0fc4bac40f5f612fbab48b7fe7c5a5f4e427
babak <babak.behzad79@gmail.com>
2023-05-27 09:01:58 -0700

    data update

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -1,9 +1,9 @@
 <template>
     <section class="experience mt-14">
         <h2>E X P E R I E N C E</h2>
-        <h3 class="mt-5 mb-1">BioLink</h3>
+        <h3 class="mt-5 mb-1">IAMDB</h3>
         <p class="medium-p mb-1">FrontEnd Developer</p>
-        <p class="small-p mb-1">BioLink is a static website with a company’s or a person’s all essential links and information on it so that user will put a single link on social media.</p>
+        <p class="small-p mb-1">IAMDB is a movie website where you search the name of any movie and have all the information about the movie displayed for you (including screen shots, trailer, simillars and more).</p>
         <h3 class="mt-3 mb-1">Tacohead</h3>
         <p class="medium-p mb-1">FrontEnd Developer</p>
         <p class="small-p">Tacohead is a static menu website. With the help of Tachohead, user of a service (like resturants, cafes, beauty salons) can see the price before ordering.</p>
